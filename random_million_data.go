ed48e8bbab1051e3aee8e5c3075cb7c4ff18dd40
Rehan-Khaliq <rehankhaliqkwl@gmail.com>
2024-08-01 10:07:44 +0500

    Update random_million_data.go

--- random_million_data.go
+++ random_million_data.go
@@ -11,44 +11,37 @@ import (
 )
 
 func main() {
-	// Creating the zip file
-	zipFile, err := os.Create("data.zip")
+	zipfile, err := os.Create("awais_data.zip") // Creation Of Zip File
 	if err != nil {
-		fmt.Println("Error creating zip file:", err)
-		return
+		fmt.Println("Found An Error To Create Zip File:", err) // if file not created show this error
+		return                                                 // return program if error found
 	}
-	defer zipFile.Close()
+	defer zipfile.Close()
+	zipwriter := zip.NewWriter(zipfile) // write data into zip file
+	defer zipwriter.Close()             // After Writing Data file closed
 
-	zipWriter := zip.NewWriter(zipFile)
-	defer zipWriter.Close()
-
-	// Creating a CSV file inside the zip
-	csvFile, err := zipWriter.Create("Million_data.csv")
+	csvfile, err := zipwriter.Create("random_data.csv") // creation of csv file is file zip
 	if err != nil {
-		fmt.Println("Error creating CSV file inside zip:", err)
-		return
+		fmt.Println("Found Error To Create CSV File:", err) // if file not created show this error
+		return                                              // Return Program If Error found
 	}
+	cswriter := csv.NewWriter(csvfile)
+	defer cswriter.Flush() // All the data stored before exiting the program
 
-	csvWriter := csv.NewWriter(csvFile)
-	defer csvWriter.Flush() // Use Flush here because we write to a zip
+	rand.Seed(time.Now().UnixNano()) // Used to generate Random number and it is very important
 
-	rand.Seed(time.Now().Unix())
-
-	// Generating and writing 1 million random numbers
 	for i := 0; i < 1000000; i++ {
-		randomNumber := generateRandomNumber()
-		if err := csvWriter.Write([]string{randomNumber}); err != nil {
-			fmt.Println("Error writing to CSV:", err)
-			return
+		randomNumber := generateRandomNumber() //generateRandomNumber is builtin function to genrate random number
+		if err := cswriter.Write([]string{randomNumber}); err != nil {
+			fmt.Println("Found Error To Store String Data Into Csv file:", err)
+			// write random number in string format into csv file
 		}
 	}
-
-	// Corrected the print statement's placement
-	fmt.Println("1 million records written successfully to Million_data.csv inside data.zip")
+	fmt.Println("1 Million Data Successfully Stored In Csv File:")
+	// Run this line outside the function if data created successfully
 }
-
 func generateRandomNumber() string {
-	firstDigit := rand.Intn(8) + 2                        // Ensures the first digit is not zero
-	remainingDigits := rand.Int63n(900000000) + 100000000 // Generates the remaining 8 digits
-	return strconv.Itoa(firstDigit) + strconv.FormatInt(remainingDigits, 10)
+	firstDigit := rand.Intn(8) + 2                  // shows number started from 2 ends on 9
+	lastDigit := rand.Int63n(900000000) + 100000000 // generate random digit that always 8 digit long
+	return strconv.Itoa(firstDigit) + strconv.FormatInt(lastDigit, 10)
 }
