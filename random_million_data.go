5cda9790bf40e2290ecd0f871282764b0d1f5c5e
Rehan-Khaliq <rehankhaliqkwl@gmail.com>
2024-08-01 10:10:26 +0500

    Update random_million_data.go

--- random_million_data.go
+++ random_million_data.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"archive/zip"
 	"encoding/csv"
 	"fmt"
 	"math/rand"
@@ -11,37 +10,30 @@ import (
 )
 
 func main() {
-	zipfile, err := os.Create("awais_data.zip") // Creation Of Zip File
-	if err != nil {
-		fmt.Println("Found An Error To Create Zip File:", err) // if file not created show this error
-		return                                                 // return program if error found
-	}
-	defer zipfile.Close()
-	zipwriter := zip.NewWriter(zipfile) // write data into zip file
-	defer zipwriter.Close()             // After Writing Data file closed
+	csvfile, err := os.Create("Million_data.csv")
 
-	csvfile, err := zipwriter.Create("random_data.csv") // creation of csv file is file zip
 	if err != nil {
-		fmt.Println("Found Error To Create CSV File:", err) // if file not created show this error
-		return                                              // Return Program If Error found
+		fmt.Println("Error Creating File", err)
+
 	}
+	defer csvfile.Close()
 	cswriter := csv.NewWriter(csvfile)
-	defer cswriter.Flush() // All the data stored before exiting the program
+	defer cswriter.Flush()
 
-	rand.Seed(time.Now().UnixNano()) // Used to generate Random number and it is very important
+	// By Applying Random Number Generator
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < 1000000; i++ { // corrected syntax errors in the loop
+		randomnumer := generateRandomNumber()
+		if err := cswriter.Write([]string{randomnumer}); err != nil {
+			fmt.Println("Error Writing To CSV:", err)
 
-	for i := 0; i < 1000000; i++ {
-		randomNumber := generateRandomNumber() //generateRandomNumber is builtin function to genrate random number
-		if err := cswriter.Write([]string{randomNumber}); err != nil {
-			fmt.Println("Found Error To Store String Data Into Csv file:", err)
-			// write random number in string format into csv file
 		}
 	}
-	fmt.Println("1 Million Data Successfully Stored In Csv File:")
-	// Run this line outside the function if data created successfully
+	fmt.Println("1 Million Data written successfully") // corrected spelling error
 }
+
 func generateRandomNumber() string {
-	firstDigit := rand.Intn(8) + 2                  // shows number started from 2 ends on 9
-	lastDigit := rand.Int63n(900000000) + 100000000 // generate random digit that always 8 digit long
-	return strconv.Itoa(firstDigit) + strconv.FormatInt(lastDigit, 10)
+	firstDigit := rand.Intn(8) + 2
+	remainingDigit := rand.Int63n(900000000) + 100000000 // 9 remaining digits
+	return strconv.Itoa(firstDigit) + strconv.FormatInt(remainingDigit, 10)
 }
